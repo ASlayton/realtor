3a9b746c6272b3aa16ecc489daec1529c0b49aea
Amanda Slayton <alslayton86@gmail.com>
2018-06-26 19:05:49 -0500

    building div created

---
+++ src/components/Building/Building.js
@@ -0,0 +1,14 @@
+import React from 'React';
+import './building.css';
+
+class Building extends React.Component {
+  render () {
+    return (
+      <div className="Building">
+        <h2>Building</h2>
+      </div>
+    );
+  }
+};
+
+export default Building;
