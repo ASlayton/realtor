817aee4ffdc8440f2c9951e9d7d9206d52811816
Amanda Slayton <alslayton86@gmail.com>
2018-07-02 19:04:25 -0500

    single building appears

--- src/components/Building/Building.js
+++ src/components/Building/Building.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {listingShape} from '../../propz/listingProp';
+import {formatPrice} from '../../helpers';
 import './Building.css';
 
 class Building extends React.Component {
+  static propTypes = {listing: listingShape};
   render () {
+    const {listing} = this.props;
+    if (!listing) {
+      return (
+        <h1>OH NO!</h1>
+      );
+    };
     return (
+
       <div className="Building">
-        <h2>Building</h2>
+        <div className="row">
+          <div className="col-xs-6">
+            <img src={listing.imageUrl} className="building-image" alt="Selected Building"/>
+          </div>
+          <div className="col-xs-6">
+            <h3>{formatPrice(listing.price)}</h3>
+            <p>{formatPrice(listing.estimatedMonthlyMorgage)}</p>
+            <h4>{listing.address} {listing.city}, {listing.state} {listing.zip}</h4>
+            <h5>{listing.numBeds} Bed/{listing.numBaths} Bath</h5>
+            <h5>{listing.squareFootage} ft<sup>2</sup></h5>
+            <p>{listing.description}</p>
+          </div>
+        </div>
+
       </div>
     );
   }
