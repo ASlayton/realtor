6628cea7326a66012730b32c96c4ffbbeea3e3b8
Amanda Slayton <alslayton86@gmail.com>
2018-07-03 19:27:49 -0500

    wip

--- src/components/Building/Building.js
+++ src/components/Building/Building.js
@@ -1,62 +1,63 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import {listingShape} from '../../propz/listingProp';
-import {formatPrice} from '../../helpers';
+
 import './Building.css';
+
 import BuildingTile from '../BuildingTile/BuildingTile';
+import {listingOptionalShape} from '../../propz/listingProp';
+import {formatPrice} from '../../helpers';
 
 class Building extends React.Component {
-  static propTypes = {listing: listingShape};
+  static propTypes = { listing: listingOptionalShape };
+
   render () {
     const {listing} = this.props;
     if (listing.nope) {
       return (
         <div className="Building">
-          <h1 className="unknown-listing"><span className="glyphicon glyphicon-arrow-left"></span>OH NO!</h1>
+          <h1 className="unknown-listing"><span className="glyphicon glyphicon-arrow-left"></span> OH NO!</h1>
         </div>
       );
-    };
+    }
     return (
       <div className="Building">
         <div className="row">
           <div className="col-xs-6">
-            <img src={listing.imageUrl} className="building-image" alt="Selected Building"/>
+            <img className="building-image" src={listing.imageUrl} alt="Selected Building"/>
           </div>
           <div className="col-xs-6">
             <h3>{formatPrice(listing.price)}</h3>
-            <p>{formatPrice(listing.estimatedMonthlyMorgage)}</p>
+            <p>{formatPrice(listing.estimatedMonthlyMorgage)} /month</p>
             <h4>{listing.address} {listing.city}, {listing.state} {listing.zip}</h4>
             <h5>{listing.numBeds} Bed/{listing.numBaths} Bath</h5>
             <h5>{listing.squareFootage} ft<sup>2</sup></h5>
             <p>{listing.description}</p>
           </div>
-          <div className="row">
-            <BuildingTile
-              imageSrc = 'cal.png'
-              altText = 'calendar'
-              pTagText = {`Built: ${listing.yearBuilt}`}
-            />
-            <BuildingTile
-              imageSrc = 'hill.png'
-              altText = 'calendar'
-              pTagText = {`${listing.lotInAcres} acres`}
-            />
-            <BuildingTile
-              imageSrc = 'flame.png'
-              altText = 'Flame'
-              pTagText = {listing.heating}
-            />
-            <BuildingTile
-              imageSrc = 'snow.png'
-              altText = 'Snowflake'
-              pTagText = {listing.cooling}
-            />
-          </div>
         </div>
-
+        <div className="row">
+          <BuildingTile
+            imageSrc = 'cal.png'
+            altText='calendar'
+            pTagText={`Built: ${listing.yearBuilt}`}
+          />
+          <BuildingTile
+            imageSrc = 'hill.png'
+            altText='hill'
+            pTagText={`${listing.lotInAcres} acres`}
+          />
+          <BuildingTile
+            imageSrc = 'flame.png'
+            altText='flame'
+            pTagText={listing.heating}
+          />
+          <BuildingTile
+            imageSrc = 'snow.png'
+            altText='snowflake'
+            pTagText={listing.cooling}
+          />
+        </div>
       </div>
     );
   }
-};
+}
 
 export default Building;
--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -2,10 +2,44 @@ import React from 'react';
 import './ListingForm.css';
 
 class ListingForm extends React.Component {
+  state = {
+    newListing: {
+      address: '',
+    },
+  }
+  addressChange = (e) => {
+    const tempListing = {...this.state.newListing};
+    tempListing.address = e.target.value;
+    this.setState({newListing: tempListing});
+  }
+
+  formSubmit = (e) => {
+    const {onSubmit} = this.props;
+    e.preventDefault();
+    onSubmit(this.state.newListing);
+  };
   render () {
+    const {newListing} = this.state;
     return (
-      <div>
-        <h2 className="ListingForm">Listing Form</h2>
+      <div className="ListingForm col-xs-8 col-xs-offset-2">
+        <h2 className="text-center">Submit new Property:</h2>
+        <form onSubmit={this.formSubmit}>
+          <div className="row">
+            <fieldset className="col-xs-3">
+              <label htmlFor="address">Address:</label>
+              <br/>
+              <input
+                className="col-xs-12"
+                type="text"
+                id="address"
+                placeholder="123 Sesame St."
+                value={newListing.address}
+                onChange={this.addressChange}
+              />
+            </fieldset>
+          </div>
+          <button className="col-xs-6 btn btn-danger col-xs-offset-3">Save House</button>
+        </form>
       </div>
     );
   }
